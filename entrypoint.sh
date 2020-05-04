14ef67dc7c03cbcd1d295cd51933c39890c6269e
Brian Lund <brian@cze.dk>
2020-05-04 14:30:46 +0200

    Debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ pip3 install flake8 pytest pytest-mock pytest-cov
 pip3 install -r $2
 
 # test
-echo "$1: " $1
-echo "$2: " $2
-echo "$3: " $3
+echo "1: " $1
+echo "2: " $2
+echo "3: " $3
 python3 -m pytest $3 --cov=$3 --cov-config=tests/.coveragerc --cov-fail-under=60
